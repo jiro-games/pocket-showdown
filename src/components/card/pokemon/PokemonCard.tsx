b45e60b05dc459772a3297e6a1cfa01ef4f127f8
jaimefd <skyjimmy7@gmail.com>
2025-10-02 15:29:20 +0200

    Implement pokemon card artworks

--- src/components/card/pokemon/PokemonCard.tsx
+++ src/components/card/pokemon/PokemonCard.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PokemonCard as PokemonCardType } from '@/types/game';
 import { getPokemonTemplate } from '@/lib/cardTemplates';
 import { EvolvesFrom } from './EvolvesFrom';
-import { CardArtwork } from '../CardArtwork';
 import { PokemonHeader } from './PokemonHeader';
 import './PokemonCard.css';
 import { useTranslations } from 'next-intl';
@@ -18,6 +17,7 @@ export function PokemonCard({ card, scale }: PokemonCardProps) {
   const tPokemon = useTranslations('pokemon');
   const cardTemplate = getPokemonTemplate(card.pokemonType);
   const stage = card.stage;
+  const [usingFallbackArtwork, setUsingFallbackArtwork] = useState(false);
 
   const isExCard = card.name.endsWith('_ex');
   const basePokemonName = isExCard ? card.name.slice(0, -3) : card.name;
@@ -26,57 +26,80 @@ export function PokemonCard({ card, scale }: PokemonCardProps) {
     card.pokemonType === 'darkness' ? 'card__content--dark' : 'card__content--light';
   const extraMoveSpace = card.attacks.some(attack => attack.cost.length > 4);
 
+  const handleArtworkError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    if (isExCard) {
+      e.currentTarget.src = `/assets/pokemon/${pokemonNameForArtwork}.png`;
+      setUsingFallbackArtwork(true);
+    }
+  };
+
   return (
     <div
-      className={`card__content ${textColorClass} ${isExCard ? 'card__content--ex' : ''}`}
+      className={`pokemon__wrapper ${isExCard ? 'card__content--ex' : ''}`}
       style={{
-        backgroundImage: `url(${cardTemplate})`,
         transform: `scale(${scale})`,
       }}
     >
-      {stage > 0 && <EvolvesFrom card={card} />}
-      {card.tags?.includes('ultra_beast') && (
-        <img src="/assets/icons/ultra-beast.webp" alt="Ultra Beast" className="card__ultra-beast" />
-      )}
-      <PokemonHeader card={card} name={tPokemon(basePokemonName)} isExCard={isExCard} />
-
-      <CardArtwork pokemonName={pokemonNameForArtwork} isExCard={isExCard} />
-
-      <div className="card__body">
-        {card.ability && <CardAbility card={card} />}
-        {card.attacks.map((attack, index) => (
-          <CardAttack key={index} attack={attack} extraSpace={extraMoveSpace} />
-        ))}
-      </div>
-      <div className="card__weakness-retreat">
-        {card.weakness && (
-          <div className="card__weakness">
-            <img
-              src={`/assets/icons/energy/${card.weakness}.webp`}
-              alt={card.weakness}
-              className="card__weakness-icon"
-            />
-            +20
-          </div>
+      <img
+        className={`pokemon__image ${usingFallbackArtwork ? 'pokemon__image--fallback' : ''}`}
+        src={`/assets/cards/${card.set}/${card.id}.png`}
+        alt={card.name}
+        onError={handleArtworkError}
+      />
+      <div
+        className={`card__content ${textColorClass}`}
+        style={{
+          backgroundImage: `url(${cardTemplate})`,
+        }}
+      >
+        {stage > 0 && <EvolvesFrom card={card} />}
+        {card.tags?.includes('ultra_beast') && (
+          <img
+            src="/assets/icons/ultra-beast.webp"
+            alt="Ultra Beast"
+            className="card__ultra-beast"
+          />
         )}
-        {card.retreatCost > 0 && (
-          <div className="card__retreat">
-            {Array.from({ length: card.retreatCost }).map((_, idx) => (
-              <img
-                key={idx}
-                src={`/assets/icons/energy/colorless.webp`}
-                alt="Colorless Energy"
-                className="card__retreat-icon"
-              />
+        <PokemonHeader card={card} name={tPokemon(basePokemonName)} isExCard={isExCard} />
+
+        <div className="card__body">
+          {card.ability && <CardAbility card={card} />}
+          <div className="card__attacks">
+            {card.attacks.map((attack, index) => (
+              <CardAttack key={index} attack={attack} extraSpace={extraMoveSpace} />
             ))}
           </div>
-        )}
-      </div>
-      <div className="card__bottom">
-        {/*<div className="card__illustrator">{card.artist}</div>*/}
-        {isExCard && (
-          <img src="/assets/icons/ex-rule.webp" alt="EX Rule" className="card__ex-rule" />
-        )}
+        </div>
+        <div className="card__weakness-retreat">
+          {card.weakness && (
+            <div className="card__weakness">
+              <img
+                src={`/assets/icons/energy/${card.weakness}.webp`}
+                alt={card.weakness}
+                className="card__weakness-icon"
+              />
+              +20
+            </div>
+          )}
+          {card.retreatCost > 0 && (
+            <div className="card__retreat">
+              {Array.from({ length: card.retreatCost }).map((_, idx) => (
+                <img
+                  key={idx}
+                  src={`/assets/icons/energy/colorless.webp`}
+                  alt="Colorless Energy"
+                  className="card__retreat-icon"
+                />
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="card__bottom">
+          {/*<div className="card__illustrator">{card.artist}</div>*/}
+          {isExCard && (
+            <img src="/assets/icons/ex-rule.webp" alt="EX Rule" className="card__ex-rule" />
+          )}
+        </div>
       </div>
     </div>
   );
